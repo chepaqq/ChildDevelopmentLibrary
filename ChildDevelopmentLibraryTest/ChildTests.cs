2fbd6c4635157cbaf31cb7e2acb4bfeab7167e72
chepaqq99 <114068192+chepaqq99@users.noreply.github.com>
2022-10-24 11:10:11 -0700

    Update ChildTests.cs

--- ChildDevelopmentLibraryTest/ChildTests.cs
+++ ChildDevelopmentLibraryTest/ChildTests.cs
@@ -6,10 +6,6 @@ namespace ChildDevelopmentLibraryTest
 {
     public class ChildTests
     {
-        [Fact]
-        public void Test1()
-        {
-            Child child;
-        }
+        
     }
 }
